4ac3090fa5f9e61c41df324c92fac6a3fcd69e63
mesamariann <145018258+mesamariann@users.noreply.github.com>
2023-09-14 05:13:54 -0700

    Add files via upload
    MARIANN MESA
    VONMAR VISCAYNO

---
+++ Mesa_Viscayno.py
@@ -0,0 +1,7 @@
+# This will print Laboratory Exercise #1
+print("Mariann Mesa & Vonmar Viscayno")
+print("Bachelor of Science in Computer Science")
+print("CS1D")
+print("18 & 19")
+print("December 08,2004 & June 19, 2004")
+print("Cogon, El Salvador City & Iponan, Cagayan de Oro City")
